c77a9f954e66a9bff67c84ae89c26925792197ab
ODSEFIANI <92064461+ODSEFIANI@users.noreply.github.com>
2023-08-08 18:41:48 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -18,13 +18,24 @@ class BaseModel():
         to_dict(self)
     """
 
-    def __init__(self):
+    def __init__(self, *arg, **kwargs):
         """
         initializes an instance
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs)!=0:
+            for key,value in kwargs.items():
+                if key=="__class__":
+                    pass
+                elif key == "created_at":
+                    self.created_at = datetime.strptime(value,"%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                      self.updated_at = datetime.strptime(value,"%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    setattr(self, key, value) 
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -45,7 +56,7 @@ class BaseModel():
     def to_dict(self):
         """
         returns a dictionary
-        containing all keys/values of __dict__ of the instance
+        containing all keys/values of __dict__ of the required instance
         """
         d = self.__dict__
         d["__class__"] = self.__class__.__name__
