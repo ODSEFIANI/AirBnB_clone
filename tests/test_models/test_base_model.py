530d92fc811674da59bf670a2163a8dd307637f0
AymanAegon <ayo12ammar24@gmail.com>
2023-08-13 16:33:28 +0100

    update test file

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,6 +7,7 @@ Unittest for BaseModel Class
 
 import unittest
 from models import base_model
+from datetime import datetime
 BaseModel = base_model.BaseModel
 
 
@@ -19,8 +20,23 @@ class TestBaseModel(unittest.TestCase):
         """
         tests if an instance is created
         """
-        obj = BaseModel()
-        self.assertTrue(isinstance(obj, BaseModel))
+        new = BaseModel()
+
+        """ check if it have methods """
+        self.assertTrue(hasattr(new, "__init__"))
+        self.assertTrue(hasattr(new, "__str__"))
+        self.assertTrue(hasattr(new, "save"))
+        self.assertTrue(hasattr(new, "to_dict"))
+
+        """existince"""
+        self.assertTrue(hasattr(new, "id"))
+        self.assertTrue(hasattr(new, "created_at"))
+        self.assertTrue(hasattr(new, "updated_at"))
+
+        """type test"""
+        self.assertIsInstance(new.id, str)
+        self.assertIsInstance(new.created_at, datetime)
+        self.assertIsInstance(new.updated_at, datetime)
 
 
 if __name__ == '__main__':
