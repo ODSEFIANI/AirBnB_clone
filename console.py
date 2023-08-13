64cf81a510026b56a97165e7007c2637349e4127
ODSEFIANI <92064461+ODSEFIANI@users.noreply.github.com>
2023-08-13 20:39:07 +0100

    Update console.py

--- console.py
+++ console.py
@@ -25,6 +25,39 @@ class HBNBCommand(cmd.Cmd):
         """ Empty line and do nothing."""
         pass
 
+    def do_show(self, arg):
+        """Pstring."""
+        ar = arg.split()
+        if not ar:
+            print("** class name missing **")
+        else:
+            c_name = ar[0]
+            if len(ar) == 0:
+                print("** class name missing **")
+            elif c_name not in HBNBCommand.cls:
+                print("** class doesn't exist **")
+            elif len(ar) < 2:
+                print("** instance id missing **")
+            else:
+                obj_key = "{}.{}".format(c_name, ar[1])
+                all_objs = storage.all()
+                if obj_key in all_objs:
+                    print(all_objs[obj_key])
+                else:
+                    print("** no instance found **")
+                    
+     def do_create(self, arg):
+        """ fresh basemodel."""
+        if not arg:
+            print("** class name missing **")
+        else:
+            try:
+                new_inst = eval(arg)()
+                new_inst.save()
+                print(new_inst.id)
+            except NameError:
+                print("** class doesn't exist **")                   
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
