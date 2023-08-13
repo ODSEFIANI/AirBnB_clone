f80999d12bfc62a2161bb85d9ffd1207a3fb4021
AymanAegon <ayo12ammar24@gmail.com>
2023-08-13 19:34:55 +0100

    5

---
+++ console.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Entry to command interpreter
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    Entry to command interpreter
+    """
+    pass
---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""test 12
+"""
+
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+import unittest
+import os
+import json
+
+
+
+class TestFileStorage(unittest.TestCase):
+
+    def test_new(self):
+        user = BaseModel()
+        self.storage.new(user)
+        all_objects = self.storage.all()
+        key = "{}.{}".format(user.__class__.__name__, user.id)
+        self.assertTrue(key in all_objects)
+
+    def test_attributes(self):
+        self.assertTrue(hasattr(self.storage, '_FileStorage__file_path'))
+        self.assertTrue(hasattr(self.storage, '_FileStorage__objects'))
+    
+    def test_reload_nonexistent_file(self):
+        
+        nonexistent_storage = FileStorage()
+        nonexistent_storage.reload()
+        self.assertEqual(nonexistent_storage.all(), {})
+
+    def test_save_reload(self):
+        user = BaseModel()
+        self.storage.new(user)
+        self.storage.save()
+        file_path = self.storage._FileStorage__file_path
+        self.assertTrue(os.path.exists(file_path))
+
+        new_storage = FileStorage()
+        new_storage.reload()
+        all_objects = new_storage.all()
+        key = "{}.{}".format(user.__class__.__name__, user.id)
+        self.assertTrue(key in all_objects)
+
+    def test_all(self):
+        all_objects = self.storage.all()
+        self.assertIsInstance(all_objects, dict)
+
+
+if __name__ == '__main__':
+    unittest.main()
