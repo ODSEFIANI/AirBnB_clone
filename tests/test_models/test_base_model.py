d25e8bd7a2b3a1d0a692e9258bf047ea8d786804
AymanAegon <ayo12ammar24@gmail.com>
2023-08-13 16:38:49 +0100

    update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,42 +1,17 @@
 #!/usr/bin/python3
-"""
-Unittest for BaseModel Class
-# run with python3 -m unittest discover tests
-# run with python3 -m unittest tests/test_models/test_base.py
-"""
-
+""" unittt test for basessss """
+import json
 import unittest
-from models import base_model
+from models.base_model import BaseModel
 from datetime import datetime
-BaseModel = base_model.BaseModel
+import models
+import sys
 
 
-class TestBaseModel(unittest.TestCase):
-    """
-    tests for models/base_model.py
-    """
 
-    def test_init(self):
-        """
-        tests if an instance is created
-        """
-        new = BaseModel()
-
-        """ check if it have methods """
-        self.assertTrue(hasattr(new, "__init__"))
-        self.assertTrue(hasattr(new, "__str__"))
-        self.assertTrue(hasattr(new, "save"))
-        self.assertTrue(hasattr(new, "to_dict"))
-
-        """existince"""
-        self.assertTrue(hasattr(new, "id"))
-        self.assertTrue(hasattr(new, "created_at"))
-        self.assertTrue(hasattr(new, "updated_at"))
-
-        """type test"""
-        self.assertIsInstance(new.id, str)
-        self.assertIsInstance(new.created_at, datetime)
-        self.assertIsInstance(new.updated_at, datetime)
+class BaseModelTestCase(unittest.TestCase):
+    """ classss for base testtt """
+    pass
 
 
 if __name__ == '__main__':
